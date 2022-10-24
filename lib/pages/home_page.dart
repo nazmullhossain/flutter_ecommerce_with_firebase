56014cf2d9c8d72f9bd03e8a53636894eaea5409
nazmullhossain <mdnazmulhossain.edu@gmail.com>
2022-10-24 09:53:24 +0600

    swiper sllider addedd in homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterecommerce/services/utils.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/dart_theme_provider.dart';
@@ -11,25 +13,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String>_offerImages=[
+    "images/offer1.jpg",
+    "images/offer2.jpg",
+    "images/offer3.jpg",
+    "images/offer4.jpg",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    final themeState=Provider.of<DarkThemeProvider>(context);
+    final Utils utils=Utils(context: context);
+    final themeState=utils.getTheme;
+    Size size=utils.getScreenSize;
     return Scaffold(
-      body: Center(
-        child: SwitchListTile(
-          title: Text("Theme"),
-          secondary:Icon(themeState.getDarkTheme?
-          Icons.dark_mode_outlined
-              : Icons.light_mode_outlined
-          ),
-          onChanged: (bool value){
-            themeState.setDarkTheme=value;
-
+      body: SizedBox(
+        height:size.height*0.33,
+        child: Swiper(
+          itemBuilder: (BuildContext context,int index){
+            return Image.asset(_offerImages[index],fit: BoxFit.fill,);
           },
-          value: themeState.getDarkTheme,
+          autoplay: true,
+          itemCount: _offerImages.length,
+          pagination: SwiperPagination(
+            alignment: Alignment.bottomCenter,
+            builder: DotSwiperPaginationBuilder(
+              color: Colors.white,
+              activeColor: Colors.red
+            )
+          ),
+          control: SwiperControl(
+            color: Colors.black
+          ),
         ),
-      ),
-    );
+      )
+      );
+
   }
 }
